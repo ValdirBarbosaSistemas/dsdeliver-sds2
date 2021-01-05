3f5df907ae68592f86a8cf1a230dd4532bebccb8
Valdir <valdir.barbosasistemas@gmail.com>
2021-01-05 12:56:11 -0300

    GET /products

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositories/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.devsuperior.dsdeliver.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsdeliver.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	/*
+	 * No código acima ele cria tudo que for relacionado ao JPA no spring como
+	 * criar, buscar, deletar entre outros...Coloca o tipo, que no caso é a entidade
+	 * que voce quer e o tipo da chave primaria.
+	 */
+	
+	// FIXME VER DEPOIS SOBRE SPRING DATA JPA - (QUERY METHODS)
+	List<Product> findAllByOrderByNameAsc();
+}
