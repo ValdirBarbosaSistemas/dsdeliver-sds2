ff12fa3b50d6747d1d79b9d53077e1979a62af38
Valdir <valdir.barbosasistemas@gmail.com>
2021-01-10 00:36:12 -0300

    Add routes and StepsHeader

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,12 @@
+import StepsHeader from './StepsHeader';
+import './styles.css';
+
+function Orders() {
+    return (
+        <div className="orders-container">
+            {/* <h1>Orders</h1> */}
+            <StepsHeader />
+        </div>
+    )
+}
+export default Orders
